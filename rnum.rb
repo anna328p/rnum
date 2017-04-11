d7107ae5ab1be7bc53739df1b9dd86eb32d6d2b7
Dmitry Kudriavtsev <me@dk0.us>
2017-04-10 19:50:32 -0700

    Wrote code

--- rnum.rb
+++ rnum.rb
@@ -1,1 +1,15 @@
 #!/usr/bin/ruby
+require 'base_x'
+
+abort "Incorrect number of arguments.\nUsage: #{File.basename __FILE__} <infile>" if ARGV.size != 1
+abort "The file #{ARGV[0]} does not exist." if not File.exist? ARGV[0]
+
+begin
+    program = File.read ARGV[0]
+rescue SystemCallError
+    abort "File #{ARGV[0]} cannot be read."
+end
+
+program.tr! '^0-9', ''
+Base10 = BaseX.new("0123456789")
+eval Base10.decode program.to_i
